5a3624779d7344b93bc2b7029b5c8e012f6d6b2b
amal555 <amalbaby5555@gmail.com>
2025-08-11 01:36:53 +0530

    db added

--- kookvid/kookvid/settings_dev.py
+++ kookvid/kookvid/settings_dev.py
@@ -70,18 +70,18 @@ WSGI_APPLICATION = 'kookvid.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-import dj_database_url
 DATABASES = {
-    'default': dj_database_url.parse(
-        "postgresql://postgres:OSwmZbhqlNZypZWPuMGyiGPbaENeasSD@shortline.proxy.rlwy.net:42179/railway"
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+# import dj_database_url
+# DATABASES = {
+#     'default': dj_database_url.parse(
+#         "postgresql://postgres:OSwmZbhqlNZypZWPuMGyiGPbaENeasSD@shortline.proxy.rlwy.net:42179/railway"
+#     )
+# }
 
 
 
